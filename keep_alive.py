bfaf192e151bf29a06c7cb9f97a2cfdc7a6d196a
xxreen <y.t.070301745678@gmail.com>
2025-05-22 10:57:50 +0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,17 +1,16 @@
-# keep_alive.py - RenderでのBot常時稼働用Webサーバー
-
 from flask import Flask
 # osモジュールは不要になりましたが、もし将来的に環境変数PORTなどを直接扱う場合は必要になるかもしれません。
 # from os import environ # 不要な場合は削除してもOK
 
 app = Flask('') # Flaskアプリケーションを初期化
 
-# ルートURL (例: Renderによって提供されるWebサービスURL) にアクセスがあった場合に
+# ルートURL (例: Railwayによって提供されるWebサービスURL) にアクセスがあった場合に
 # 「Bot is alive!」というテキストを返します。
 @app.route('/')
 def home():
     return "Bot is alive!"
 
-# RenderではgunicornなどのWSGIサーバーで起動するため、
-# Replitのようにスレッドで起動する関数や、app.run()を直接呼び出す必要はありません。
+# RailwayではgunicornなどのWSGIサーバーで起動するため、
+# app.run()を直接呼び出す必要はありません。
 # gunicornがこのファイル内の 'app' オブジェクトを自動的に見つけて起動します。
+
